4e219a23b48a650f1848bb017f15076946185333
Ether2k25 <etherice2k25@gmail.com>
2025-08-31 15:59:24 +0530

    Update and rename mongodb.js to mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,24 +1,29 @@
+// lib/mongodb.ts
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI as string;
 const options = {};
-
-let client;
-let clientPromise;
 
 if (!uri) {
   throw new Error("Please add your Mongo URI to Environment Variables");
 }
 
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // allow global var for hot-reload in dev
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 if (process.env.NODE_ENV === "development") {
-  // In dev, use a global variable so it doesn’t re-init every time
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In prod, create a new connection each time
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
